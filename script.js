027a9c20a92a50eef44707a4b60877b03fe6e366
Ressuu <dsmoczyk34@gmail.com>
2023-07-31 23:09:56 +0100

    Upadte

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
-import { db } from "/firebase.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-analytics.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  collection,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAcaFgej-eDFC4L_RdMGGJvxmHrylbKnxQ",
+  authDomain: "todoapp-4a472.firebaseapp.com",
+  projectId: "todoapp-4a472",
+  storageBucket: "todoapp-4a472.appspot.com",
+  messagingSenderId: "749324792249",
+  appId: "1:749324792249:web:261d99bf4d6e6b633c8668",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+//koniec firebase
+
+let text = document.getElementById("todo-input");
 function addItem(event) {
-  console.log(event);
-  let text = document.getElementById("todo-input");
-
-  try {
-    const docRef = addDoc(collection(db, "todo-items"), {
-      text: text.value,
-      status: "active",
-    });
-    console.log("Document written with ID: ", docRef.text);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
+  event.preventDefault();
+  const docRef = addDoc(collection(db, "To-do"), {
+    text: text.value,
+    status: "active",
+  });
+  console.log("Document written with ID: ", docRef.id);
 }
+
+text.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    addItem(e);
+  }
+});
