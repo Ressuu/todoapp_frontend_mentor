4ac92a6a28b880c875183827cf697cd7f70d75e5
Ressuu <dsmoczyk34@gmail.com>
2023-08-01 22:31:52 +0100

    update

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-auth
 import {
   getFirestore,
   doc,
+  getDoc,
   setDoc,
+  getDocs,
   collection,
+  updateDoc,
   addDoc,
+  onSnapshot,
 } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
@@ -23,7 +27,10 @@ const db = getFirestore(app);
 
 //koniec firebase
 
+let items = [];
 let text = document.getElementById("todo-input");
+const todoCollectionRef = collection(db, "To-do");
+
 function addItem(event) {
   event.preventDefault();
   const docRef = addDoc(collection(db, "To-do"), {
@@ -31,10 +38,85 @@ function addItem(event) {
     status: "active",
   });
   console.log("Document written with ID: ", docRef.id);
+  text.value = "";
 }
 
 text.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
     addItem(e);
   }
+});
+
+async function getItems() {
+  const querySnapshot = await getDocs(collection(db, "To-do"));
+  const items = [];
+  querySnapshot.forEach((doc) => {
+    items.push({
+      id: doc.id,
+      ...doc.data(),
+    });
+  });
+  return items;
+}
+
+function generateItems(items) {
+  let itemsHTML = "";
+  items.forEach((item) => {
+    itemsHTML += `
+      <div class="todo-item">
+        <div class="check">
+          <div data-id="${item.id}" class="check-mark ${
+      item.status == "completed" ? "checked" : ""
+    }">
+            <img src="/images/icon-check.svg" alt="" />
+          </div>
+        </div>
+        <div class="todo-text ${item.status == "completed" ? "checked" : ""}">
+          ${item.text}
+        </div>
+      </div>   
+    `;
+  });
+
+  document.querySelector(".todo-items").innerHTML = itemsHTML;
+  createEventListeners();
+}
+
+function createEventListeners() {
+  let toDoCheckMarks = document.querySelectorAll(".todo-item .check-mark");
+  toDoCheckMarks.forEach((checkMark) => {
+    checkMark.addEventListener("click", function () {
+      markCompleted(checkMark.dataset.id);
+    });
+  });
+}
+async function markCompleted(id) {
+  const itemRef = doc(db, "To-do", id);
+  const itemSnapshot = await getDoc(itemRef);
+  const itemData = itemSnapshot.data();
+
+  if (itemData.status === "completed") {
+    await updateDoc(itemRef, { status: "active" });
+  } else {
+    await updateDoc(itemRef, { status: "completed" });
+  }
+}
+
+function listenToTodoChanges() {
+  onSnapshot(todoCollectionRef, (querySnapshot) => {
+    const items = [];
+    querySnapshot.forEach((doc) => {
+      items.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+    generateItems(items);
+  });
+}
+
+listenToTodoChanges();
+
+getItems().then((items) => {
+  generateItems(items);
 });
